6f135cabba8df822bfbef602d387931b00cf9fe0
niyongwei <1500920628@qq.com>
2022-04-24 17:19:09 +0800

    init alert 0.0.1

--- src/.umi/dumi/demos/index.ts
+++ src/.umi/dumi/demos/index.ts
@@ -3,9 +3,9 @@ import React from 'react';
 import { dynamic } from 'dumi';
 import rawCode1 from '!!dumi-raw-code-loader!D:/git-work/components/src/alert/demo/basic.tsx?dumi-raw-code';
 import rawCode2 from '!!dumi-raw-code-loader!D:/git-work/components/src/alert/index.tsx?dumi-raw-code';
-import rawCode3 from '!!dumi-raw-code-loader!D:/git-work/components/src/common-provider/context.tsx?dumi-raw-code';
-import rawCode4 from '!!dumi-raw-code-loader!D:/git-work/components/src/alert/style/index.ts?dumi-raw-code';
-import rawCode5 from '!!dumi-raw-code-loader!D:/git-work/components/src/alert/style/index.less?dumi-raw-code';
+import rawCode3 from '!!dumi-raw-code-loader!D:/git-work/components/src/alert/style/index.ts?dumi-raw-code';
+import rawCode4 from '!!dumi-raw-code-loader!D:/git-work/components/src/alert/style/index.less?dumi-raw-code';
+import rawCode5 from '!!dumi-raw-code-loader!D:/git-work/components/src/common-provider/context.tsx?dumi-raw-code';
 import rawCode6 from '!!dumi-raw-code-loader!D:/git-work/components/src/input/demo/input.tsx?dumi-raw-code';
 import rawCode7 from '!!dumi-raw-code-loader!D:/git-work/components/src/input/index.tsx?dumi-raw-code';
 import rawCode8 from '!!dumi-raw-code-loader!D:/git-work/components/src/input/input.tsx?dumi-raw-code';
@@ -21,14 +21,14 @@ export default {
       loader: async () => (await import(/* webpackChunkName: "demos_trela" */'D:/git-work/components/src/alert/demo/basic.tsx')).default,
       loading: () => null,
     }),
-    previewerProps: {"sources":{"_":{"tsx":rawCode1},"index.tsx":{"import":"..","content":rawCode2},"common-provider/context.tsx":{"import":"../common-provider/context","content":rawCode3},"style/index.ts":{"import":"../style","content":rawCode4},"style/index.less":{"import":"./index.less","content":rawCode5}},"dependencies":{"react":{"version":"17.0.2"},"prop-types":{"version":"15.8.1"}},"componentName":"alert","identifier":"alert-basic"},
+    previewerProps: {"sources":{"_":{"tsx":rawCode1},"index.tsx":{"import":"..","content":rawCode2},"style/index.ts":{"import":"../style","content":rawCode3},"style/index.less":{"import":"./index.less","content":rawCode4},"common-provider/context.tsx":{"import":"../common-provider/context","content":rawCode5}},"dependencies":{"react":{"version":"17.0.2"},"prop-types":{"version":"15.8.1"},"classnames":{"version":"2.3.1"}},"componentName":"alert","identifier":"alert-basic"},
   },
   'input-input': {
     component: dynamic({
       loader: async () => (await import(/* webpackChunkName: "demos_tupni" */'D:/git-work/components/src/input/demo/input.tsx')).default,
       loading: () => null,
     }),
-    previewerProps: {"sources":{"_":{"tsx":rawCode6},"index.tsx":{"import":"..","content":rawCode7},"input.tsx":{"import":"./input","content":rawCode8},"BasicInput.tsx":{"import":"./BasicInput","content":rawCode9},"common-provider/context.tsx":{"import":"../common-provider/context","content":rawCode3},"inputNumber.tsx":{"import":"./inputNumber","content":rawCode10},"style/index.ts":{"import":"./style","content":rawCode11},"style/index.less":{"import":"./index.less","content":rawCode12}},"dependencies":{"react":{"version":">=16.9.0"},"rc-util":{"version":"5.21.0"},"react-dom":{"version":">=16.9.0"},"rc-input-number":{"version":"7.3.4"}},"componentName":"input","identifier":"input-input"},
+    previewerProps: {"sources":{"_":{"tsx":rawCode6},"index.tsx":{"import":"..","content":rawCode7},"input.tsx":{"import":"./input","content":rawCode8},"BasicInput.tsx":{"import":"./BasicInput","content":rawCode9},"common-provider/context.tsx":{"import":"../common-provider/context","content":rawCode5},"inputNumber.tsx":{"import":"./inputNumber","content":rawCode10},"style/index.ts":{"import":"./style","content":rawCode11},"style/index.less":{"import":"./index.less","content":rawCode12}},"dependencies":{"react":{"version":">=16.9.0"},"rc-util":{"version":"5.21.0"},"react-dom":{"version":">=16.9.0"},"rc-input-number":{"version":"7.3.4"}},"componentName":"input","identifier":"input-input"},
   },
   'upload-basic': {
     component: dynamic({
--- src/alert/demo/basic.tsx
+++ src/alert/demo/basic.tsx
@@ -4,22 +4,31 @@
  * @LastEditors: niyongwei
  * @LastEditTime: 2022-04-07 10:48:31
  * @FilePath: \git-work\components\src\alert\demo\basic.tsx
- * @Description: 
- * 
- * Copyright (c) 2022 by nyw, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2022 by nyw, All Rights Reserved.
  */
-import React from 'react';
-import Alert from '..';
+import React from 'react'
+import Alert from '..'
 import ConfigProvider from '../../common-provider/context'
-import '../style';
+import '../style'
 
 export default () => {
-    return (
-        <ConfigProvider locale={'asd'}>
-            <Alert kind="warning">
-                <div>这是一条警告提示?</div>
-                <span>1</span>
-            </Alert>
-        </ConfigProvider>
-    )
-};
+  const onClose = () => {
+    console.log('关闭中')
+  }
+  return (
+    <ConfigProvider locale={'asd'}>
+      <Alert
+        kind="warning"
+        closeable
+        close={<span>delete</span>}
+        // duration={3000}
+        onClose={onClose}
+      >
+        <div>这是一条警告提示?</div>
+        <span>1</span>
+      </Alert>
+    </ConfigProvider>
+  )
+}
