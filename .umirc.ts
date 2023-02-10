236aa36253e40607c913349784d3bd58970f63fa
niyongwei <1500920628@qq.com>
2023-02-10 13:09:25 +0800

    feat: 去除拆包限制

--- .umirc.ts
+++ .umirc.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   mode: 'site',
   outputPath: 'doc-site', // 输出文件夹
   base: '/components/',
-  exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
-  dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
+  // exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
+  // dynamicImport: {}, // 拆包 站点过大时可以优化首屏加载速度
 });
